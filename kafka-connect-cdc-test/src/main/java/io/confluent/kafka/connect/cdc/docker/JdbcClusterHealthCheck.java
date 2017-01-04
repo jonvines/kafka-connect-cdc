8a428f2e8a0e95221c3ebb90e30d80a727b62005
Jeremy Custenborder <jcustenborder@gmail.com>
2017-01-04 02:23:11 -0600

    Throw an exception if the container is not in the up state.

--- kafka-connect-cdc-test/src/main/java/io/confluent/kafka/connect/cdc/docker/JdbcClusterHealthCheck.java
+++ kafka-connect-cdc-test/src/main/java/io/confluent/kafka/connect/cdc/docker/JdbcClusterHealthCheck.java
@@ -1,14 +1,17 @@
 package io.confluent.kafka.connect.cdc.docker;
 
+import com.google.common.base.Preconditions;
 import com.palantir.docker.compose.connection.Cluster;
 import com.palantir.docker.compose.connection.Container;
 import com.palantir.docker.compose.connection.DockerPort;
+import com.palantir.docker.compose.connection.State;
 import com.palantir.docker.compose.connection.waiting.Attempt;
 import com.palantir.docker.compose.connection.waiting.ClusterHealthCheck;
 import com.palantir.docker.compose.connection.waiting.SuccessOrFailure;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 
@@ -31,11 +34,19 @@ public class JdbcClusterHealthCheck implements ClusterHealthCheck {
 
   @Override
   public SuccessOrFailure isClusterHealthy(Cluster cluster) {
+    final Container container = cluster.container(containerName);
+    try {
+      Preconditions.checkState(State.Up == container.state(), "container %s should be up", containerName);
+    } catch (IOException e) {
+      log.error("exception thrown", e);
+    } catch (InterruptedException e) {
+      log.error("exception thrown", e);
+    }
     return SuccessOrFailure.onResultOf(
         new Attempt() {
           @Override
           public boolean attempt() throws Exception {
-            final Container container = cluster.container(containerName);
+
             final DockerPort dockerPort = container.port(port);
             final String jdbcUrl = dockerPort.inFormat(jdbcUrlFormat);
 
