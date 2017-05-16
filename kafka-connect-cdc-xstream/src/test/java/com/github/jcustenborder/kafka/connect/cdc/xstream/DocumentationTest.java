e9284ffc0f463ff468478245677221c1a07eb3a5
Jeremy Custenborder <jcustenborder@gmail.com>
2017-05-16 09:20:44 -0500

    Refactored config and documentation.

--- kafka-connect-cdc-xstream/src/test/java/com/github/jcustenborder/kafka/connect/cdc/xstream/DocumentationTest.java
+++ kafka-connect-cdc-xstream/src/test/java/com/github/jcustenborder/kafka/connect/cdc/xstream/DocumentationTest.java
@@ -5,7 +5,7 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- * http://www.apache.org/licenses/LICENSE-2.0
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -13,15 +13,13 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.github.jcustenborder.kafka.connect.cdc.mssql;
+package com.github.jcustenborder.kafka.connect.cdc.xstream;
 
-import com.github.jcustenborder.kafka.connect.utils.config.MarkdownFormatter;
-import org.junit.jupiter.api.Test;
+import com.github.jcustenborder.kafka.connect.utils.BaseDocumentationTest;
 
-public class MsSqlSourceConnectorConfigTest {
-  @Test
-  public void doc() {
-    System.out.println(MarkdownFormatter.toMarkdown(MsSqlSourceConnectorConfig.config()));
+public class DocumentationTest extends BaseDocumentationTest {
+  @Override
+  protected String[] packages() {
+    return new String[]{this.getClass().getPackage().getName()};
   }
-
 }
